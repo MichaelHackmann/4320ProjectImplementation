2d698eb4ced773be3f59758ae5305f34d1f13aab
Michael Hackmann <mjhackmann@icloud.com>
2024-04-07 14:10:00 -0500

    Add quantity

--- customerApp.js
+++ customerApp.js
@@ -114,14 +114,65 @@ router.post('/addToCart', async (req, res) => {
     let query;
     switch(action) {
         case "ADD_TO_CART":
-            query = `INSERT INTO ShoppingCartProduct (cartID, productID)
+            query = `INSERT INTO ShoppingCartProduct (cartID, productID, productQty)
                     VALUES
-                    (1, '${inputData[1]}');`;
+                    (1, '${inputData[1]}', 1);`;
 
             break;
     }
     let addToCartPromise = mysqlQueryRunner(conn, query);
     addToCartPromise.then(result => console.log(`${action} SUCCESS: product with ID "${inputData[1]}" succesfully added to cart`))
+    .catch(err => console.log(err))
+
+    conn.end((err) => {
+        if (err) {
+            console.error('Error closing the database connection:', err.stack);
+            return;
+        }
+        console.log('Database connection closed');
+    });
+});
+
+router.post('/updateCartQuantity', async (req, res) => {
+    let conn = connectionrequest();
+    
+    const inputData = req.body.inputData;
+
+    let productID = inputData[0];
+    let newQty = inputData[1];
+    let query;
+
+    query = `UPDATE ShoppingCartProduct
+            SET productQty = ${newQty}
+            WHERE cartID = 1 AND productID = ${productID}`;
+
+    let addToCartPromise = mysqlQueryRunner(conn, query);
+    addToCartPromise.then(result => console.log(`UPDATE_QTY SUCCESS: product quantity with ID "${inputData[0]}" succesfully updated`))
+    .catch(err => console.log(err))
+
+    conn.end((err) => {
+        if (err) {
+            console.error('Error closing the database connection:', err.stack);
+            return;
+        }
+        console.log('Database connection closed');
+    });
+});
+
+router.post('/incrementCartQuantity', async (req, res) => {
+    let conn = connectionrequest();
+    
+    const inputData = req.body.inputData;
+
+    let productID = inputData[0];
+    let query;
+
+    query = `UPDATE ShoppingCartProduct
+            SET productQty = productQty + 1
+            WHERE cartID = 1 AND productID = ${productID}`;
+
+    let addToCartPromise = mysqlQueryRunner(conn, query);
+    addToCartPromise.then(result => console.log(`INC_QTY SUCCESS: product quantity with ID "${inputData[0]}" succesfully incremented`))
     .catch(err => console.log(err))
 
     conn.end((err) => {
